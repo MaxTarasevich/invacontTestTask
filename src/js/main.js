ef3436d7610535ad38eeea231c03220fc52cfe52
Max <maxtarasevich@gmail.com>
2021-05-24 16:32:41 +0300

    add media for header menu, add js for burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,51 @@
-console.log(`hello word`)
+const mediaQuery = window.matchMedia('(max-width: 700px)')
+
+const burgerMenu = document.querySelector(`.header_burger-menu`)
+const nav = document.querySelector(`.nav`)
+const links = document.querySelectorAll(`.header .nav li`)
+
+
+
+
+ 
+burgerMenu.addEventListener(`click`, () => {
+
+    if(mediaQuery.matches){
+
+        if(nav.classList.contains(`active`)){
+            setTimeout(() => {
+                nav.classList.remove(`active`)
+            },1100)
+            nav.style.top = `-100vh`
+        }else{
+            nav.classList.toggle(`active`)
+            setTimeout(()=> {
+                nav.style.top = `0`
+            },10)
+        }
+    }
+})
+
+links.forEach((link) => {
+    link.addEventListener(`click`, () => {
+        if(mediaQuery.matches){
+
+            if(nav.classList.contains(`active`)){
+                setTimeout(() => {
+                    nav.classList.remove(`active`)
+                },1100)
+                nav.style.top = `-100vh`
+            }else{
+                nav.classList.toggle(`active`)
+                setTimeout(()=> {
+                    nav.style.top = `0`
+                },10)
+            }
+        }
+    })
+});
+
+
+
+
+
